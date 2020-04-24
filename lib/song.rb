920448f368d0f38fdc3fbd4ab43dafec48e39977
Trevor Low <tlow94@uw.edu>
2020-04-24 05:17:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,14 @@ class Song
   end
   genre_count
 end
+def self.artist_count
+  artist_count = {}
+  @@artists.select do |artist|
+    if artist_count[artist]
+      artist_count[artist] +=1
+    else
+      artist_count[artist]
+    end
+  end
+end
 end
